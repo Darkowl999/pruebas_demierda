68380e53af0266a5ecfc47eec8ede446445d20e2
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2020-11-25 19:57:22 -0400

    iniciamos este proyecto de prueba

---
+++ android/app/src/main/kotlin/com/example/pruebas_demierda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pruebas_demierda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/otraprueba.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class prueba extends StatefulWidget {
+  prueba({Key key}) : super(key: key);
+
+  @override
+  _pruebaState createState() => _pruebaState();
+}
+
+class _pruebaState extends State<prueba> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
